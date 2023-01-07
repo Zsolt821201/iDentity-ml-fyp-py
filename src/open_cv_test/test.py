bec182b416ef4ee010f881c4c6cad0d79cd7725a
Zsolt <57398867+Zsolt821201@users.noreply.github.com>
2023-01-07 11:40:26 +0000

    Working images example

--- src/open_cv_test/test.py
+++ src/open_cv_test/test.py
@@ -2,25 +2,26 @@ import cv2
 import sys
 
 # Get user supplied values
-#abba.png pic, and the haarcascade_frontalface_default.xml
-imagePath = sys.argv[1]
-cascPath = "haarcascade_frontalface_default.xml"
+imagePath = "src/open_cv_test/abba.png"# sys.argv[1]
+cascPath = "src/open_cv_test/haarcascade_frontalface_default.xml"
 
 # Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
+cascadeClassifier = cv2.CascadeClassifier(cascPath)
 
 # Read the image
 image = cv2.imread(imagePath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Detect faces in the image
-faces = faceCascade.detectMultiScale(
+faces = cascadeClassifier.detectMultiScale(
     gray,
     scaleFactor=1.1,
     minNeighbors=5,
     minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    flags = cv2.CASCADE_SCALE_IMAGE
 )
+
+print(faces)
 
 print("Found {0} faces!".format(len(faces)))
 
@@ -29,4 +30,5 @@ for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+#cv2.waitKey(0)
+

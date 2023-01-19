6caa6eb68f7eab8d7e4fd28f0252adf3094dc23b
Zsolt <57398867+Zsolt821201@users.noreply.github.com>
2023-01-19 12:09:18 +0000

    Model

--- identity-django/identity/models.py
+++ identity-django/identity/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    id = models.AutoField(primary_key=True)
+    address = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, unique=True)
+    name = models.CharField(max_length=200, unique=True)
+    telephone = models.em(max_length=20)
+
+class LocationPermission(models.Model):
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    user_account_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+        
+
+class Roster(models.Model):
+    id = models.AutoField(primary_key=True)
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    sign_in_date = models.DateTimeField('date signed in', auto_now_add=True)
+    sign_out_date = models.DateTimeField('date signed out', auto_now_add=True)
+    user_account_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+

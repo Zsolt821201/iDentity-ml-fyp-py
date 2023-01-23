95d64becd204e33f27da8b0343757e585ecf0f39
Zsolt <57398867+Zsolt821201@users.noreply.github.com>
2023-01-23 21:29:24 +0000

    image  upload WIP

--- identity-django/identity/models.py
+++ identity-django/identity/models.py
@@ -1,3 +1,14 @@
+from dataclasses import dataclass
+import datetime
+from numpy import ndarray
+from pathlib import Path
+import os
+import sqlite3
+from sqlite3 import Error
+
+import cv2
+import numpy
+from PIL import Image
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -41,3 +52,55 @@ class UserAccount(AbstractUser):
     
     def __str__(self):
         return self.get_username()
+   
+
+CLASSIFIER_CONFIGURATION: str = str(Path(
+    __file__).parent / 'haarcascades/haarcascade_frontalface_default.xml')
+DATABASE_DIRECTORY: str = str(Path(
+    __file__).parent.parent / 'database/')
+DATABASE_FACE_DIRECTORY: str = str(Path(
+    __file__).parent.parent / 'database/identity_face_dataset')
+DATABASE_FACIAL_TRAINER: str = str(Path(
+    __file__).parent.parent / 'database/trainer.yml')
+
+   
+def get_images_and_labels(path):
+
+    image_paths = [os.path.join(path, f) for f in os.listdir(path)]
+    face_samples = []
+    ids = []
+
+    detector = cv2.CascadeClassifier(CLASSIFIER_CONFIGURATION)
+
+    for image_path in image_paths:
+
+        image = Image.open(image_path).convert(
+            'L')  # convert it to grayscale
+        img_numpy = numpy.array(image, 'uint8')
+
+        user_id = int(os.path.split(image_path)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_numpy)
+
+        for (x, y, w, h) in faces:
+            face_samples.append(img_numpy[y:y+h, x:x+w])
+            ids.append(user_id)
+
+    return face_samples, ids 
+    
+def face_training():
+    print("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+    faces, ids = get_images_and_labels(DATABASE_FACE_DIRECTORY)
+
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    recognizer.train(faces, numpy.array(ids))
+
+    # Save the model into trainer/trainer.yml
+    # recognizer.save() worked on Mac, but not on Pi
+    recognizer.write(DATABASE_FACIAL_TRAINER)
+
+    # Print the number of faces trained and end program
+    print("\n [INFO] {0} faces trained. Exiting Program".format(
+        len(numpy.unique(ids))))
+    
+    
--- identity-django/identity/views.py
+++ identity-django/identity/views.py
@@ -1,13 +1,12 @@
+import cv2
 from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse
-from .models import Location
+from .models import Location, UserAccount
 
 def index(request):
     return render(request, 'website/index.html')
 
-def setup_facial_recognition(request):
-    return render(request, 'user-accounts/setup-facial-recognition.html')
 
 def locations(request):
     locations: list(Location) = Location.objects.order_by('-name')
@@ -16,4 +15,32 @@ def locations(request):
 def location_details(request, location_id):
     location = get_object_or_404(Location, pk=location_id)
     return render(request, 'locations/details.html', {'location': location})
+
+def setup_facial_recognition(request):
+    return render(request, 'user-accounts/setup-facial-recognition.html')
+
+def test(request):
+   return render(request, 'user-accounts/test.html')
+
+def upload_facial_data(request):
+    data = request.data
+    # get loggedInUser
+    user_account_id:int = 1
+
+    if user_account_id != data['user-account-id']:
+        return HttpResponse('Unauthorized', status=401)
+    
+    user_account = get_object_or_404(UserAccount, pk=user_account_id)
+    face_images = data['images']
+    user_account.is_face_recognition_enabled = True
+    UserAccount.save(user_account, face_images)
+    
+    face_count: int = 0
+    
+    for face_image in face_images:
+        image_path: str = f"{DATABASE_FACE_DIRECTORY}/user.{user_account_id}.{face_count}.jpg"
+        cv2.imwrite(image_path, face_image)
+
+    return HttpResponse('OK', status=200)
+    
 

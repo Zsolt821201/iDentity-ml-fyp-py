577ada89f0343433394dc9d2d9f02eff863e5a7f
Zsolt <57398867+Zsolt821201@users.noreply.github.com>
2023-01-23 21:28:41 +0000

    open.js

--- identity-django/identity/urls.py
+++ identity-django/identity/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     #path('roaster-logs/', views.roaster_logs, name='roaster_logs'),
     path('setup-facial-recognition/', views.setup_facial_recognition, name='setup_facial_recognition'),
     path('locations/', views.locations, name='locations'),
-    path('locations/<int:location_id>/',
-         views.location_details, name='details'),
+    path('test/', views.test, name='test'),
+    path('locations/<int:location_id>/',views.location_details, name='details'),
+    path('upload-facial-data/', views.upload_facial_data, name="upload-facial-data"),
 ]

2a5e258f6c31202d7c633ce5a1487022e2b0dd21
Zsolt <57398867+Zsolt821201@users.noreply.github.com>
2023-02-06 18:32:30 +0000

    javascript clean up 2/3

--- identity-django/identity/static/js/facial-login.js
+++ identity-django/identity/static/js/facial-login.js
@@ -1,8 +1,11 @@
-class UrlPaths
-{
+/**
+ * 
+ */
+class UrlPaths {
     static get UPLOAD_FACIAL_DATA_URL() { return "/upload-facial-data/"; }
     static get PERFORM_SIGN_IN_URL() { return "/perform-sign-in/"; }
 }
+
 
 async function streamSignInVideo() {
     //TODO: Implement facial login
@@ -13,8 +16,7 @@ async function streamSignInVideo() {
 
     const csrfToken = "";//document.querySelector('[name=csrfmiddlewaretoken]').value;
     let responseCode;
-    do 
-    {
+    do {
 
         var canvas = document.createElement("canvas");
         canvas.getContext('2d').drawImage(videoElement, 0, 0, canvas.width, canvas.height);
@@ -29,13 +31,21 @@ async function streamSignInVideo() {
     }
     while (responseCode == 418)
 
-    if(responseCode == 200)
+    if (responseCode == 200)
         alert("signed in completed");
-        //redirectUserToDashboard();
+    //redirectUserToDashboard();
     else // responseCode == ERROR/500
         console.log("Error: " + responseCode);
 }
 
+/**
+ * 
+ * @param {String} imageBase64Encoding  The base64 encoding of the image to upload
+ * @param {Number} imageNumber The number of the image being uploaded
+ * @param {Number} locationId The id of the location the user is signing in at
+ * @param {String} csrfToken The CSRF token for the request
+ * @returns 
+ */
 function buildSignInOptions(imageBase64Encoding, imageNumber, locationId, csrfToken) {
     const formData = new FormData();
     const headers = {
@@ -70,42 +80,38 @@ async function executeRequest(url, options) {
     return responseCode;
 }
 
-
-
-function createFileFromUrl(url, filename) {
-    let request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function (_) {
-        if (request.readyState === 4) {
-            if (request.status === 200) {
-                let data = new Uint8Array(request.response);
-                let thing = cv.FS_createDataFile('/', filename, data, true, false, false);
-                console.log(thing)
-            } else {
-                console.error('Failed to load ' + url + ' status: ' + request.status);
-            }
-        }
-    };
-    request.send();
-};
-
-const VideoResolutionFormats = {
+/**
+ * @typedef {String} VideoResolutionFormatNames
+ */
+const VideoResolutionFormatNames = {
     QVGA: "qvga",
-    VGA: "vga",
+    VGA:  "vga",
 };
 
+/**
+ * @typedef {Object} VideoResolutionFormat
+ */
+const VideoResolutionFormats = {
+    /**
+     * @type {VideoResolutionFormat}
+     */
+    'qvga': { width: { exact: 320 }, height: { exact: 240 } },
+    'vga':  { width: { exact: 640 }, height: { exact: 480 } },
+};
+
+/**
+ * Starts the camera and displays the video stream without audio on the video element.  If the video element does not exist, it will be created.
+ * @param {VideoResolutionFormatNames} resolution 
+ * @param {String} videoId The HTML element id of the video element to display the video stream on.
+ */
 function startCamera(resolution, videoId) {
-    const constraints = {
-        'qvga': { width: { exact: 320 }, height: { exact: 240 } },
-        'vga': { width: { exact: 640 }, height: { exact: 480 } }
-    };
+
     let videoElement = document.getElementById(videoId);
     if (!videoElement) {
         videoElement = document.createElement('video');
     }
 
-    let videoConstraint = constraints[resolution];
+    let videoConstraint = VideoResolutionFormats[resolution];
     if (!videoConstraint) {
         videoConstraint = true;
     }
@@ -119,70 +125,3 @@ function startCamera(resolution, videoId) {
             console.error('Camera Error: ' + err.name + ' ' + err.message);
         });
 }
-function setUpFacialRecognition() {
-    let begin = Date.now();
-
-    let faceCascadeFile = "haarcascade_frontalface_default.xml"
-    let faceCascadeFileUrl = "https://raw.githubusercontent.com/kipr/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
-    //let faceCascadeFileUrl  ="http://127.0.0.1:8000/static/haarcascade_frontalface_default.xml"
-
-    createFileFromUrl(faceCascadeFileUrl, faceCascadeFile);
-
-    const CLASSIFIER_CONFIGURATION = "https://raw.githubusercontent.com/kipr/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml";//"haarcascade_frontalface_default.xml";
-    let videoInputElement = document.getElementById('videoInput');
-
-
-    let videoCapture = new cv.VideoCapture(videoInputElement);
-    let faceDetectorClassifier = new cv.CascadeClassifier();
-    faceDetectorClassifier.load(faceCascadeFile);
-
-
-    let src = new cv.Mat(videoInputElement.height, videoInputElement.width, cv.CV_8UC4);
-
-    let canvasOutputElement = document.getElementById('canvasOutput');
-    let dst = new cv.Mat(canvasOutputElement.height, canvasOutputElement.width, cv.CV_8UC4);
-
-
-    const FPS = 30;
-
-    let faceImages = [];
-    let face_count = 0;
-
-    while (face_count < FACE_SAMPLE_COUNT) {
-        videoCapture.read(src);
-        src.copyTo(dst);
-
-        let gray_scale_image = new cv.Mat();
-        cv.cvtColor(dst, gray_scale_image, cv.COLOR_RGBA2GRAY, 0);
-
-        try {
-            let faces = new cv.RectVector();
-            faceDetectorClassifier.detectMultiScale(gray_scale_image, faces, 1.1, 3, 0);
-
-            if (faces.size() == 0) {
-                console.log("Error: No face detected");
-            }
-
-            if (faces.size() > 1) {
-                console.log("Error: More than one face detected");
-            }
-
-            // draw faces.
-            for (const face of faces) {
-                let point1 = new cv.Point(face.x, face.y);
-                let point2 = new cv.Point(face.x + face.width, face.y + face.height);
-                cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
-                face_count += 1
-                faceImages.push(dst);
-            }
-            cv.imshow('canvasOutput', dst);
-        }
-        catch (err) {
-            console.log(err);
-        }
-        // schedule the next one.
-        let delay = 1000 / FPS - (Date.now() - begin);
-        setTimeout(setUpFacialRecognition, delay);
-    };
-}
-
